0eb42fed74a7e4c8e691dc6a5411c78869c19438
Carina Semerdzhan <carina@legartis.ai>
2022-10-13 14:58:04 +0200

    adds test and fix lint

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,14 +16,29 @@ class UserAdmin(BaseUserAdmin):
             _('Permissions'),
             {
                 'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser'
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
                 )
             }
         ),
-        (_('Important dates', {'fields': ('last_login')}))
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
